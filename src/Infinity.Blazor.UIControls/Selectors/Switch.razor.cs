a78e27b906ff752130e43a0b1d6926c177154e0b
DoTin <doquoctin94@gmail.com>
2023-01-04 15:51:57 +0700

    add checkbox and switch

---
+++ src/Infinity.Blazor.UIControls/Selectors/Switch.razor.cs
@@ -0,0 +1,46 @@
+﻿using Infinity.Blazor.UIControls.Icons;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+
+namespace Infinity.Blazor.UIControls.Selectors;
+
+partial class Switch
+{
+    [Parameter]
+    public string Id { get; set; }
+
+    [Parameter]
+    public string SpriteClass { get; set; }
+
+    [Parameter]
+    public bool Enabled { get; set; } = true;
+
+    [Parameter]
+    public int? TabIndex { get; set; }
+
+    [Parameter]
+    public string Width { get; set; }
+
+    private bool _value;
+
+    [Parameter]
+    public bool Value
+    {
+        get => _value;
+        set
+        {
+            if (!EqualityComparer<bool>.Default.Equals(value, _value))
+            {
+                _value = value;
+                ValueChanged.InvokeAsync(value);
+            }
+        }
+    }
+
+    [Parameter]
+    public EventCallback<bool> ValueChanged { get; set; }
+
+
+    [Parameter]
+    public EventCallback OnChangeHandler { get; set; }
+}
