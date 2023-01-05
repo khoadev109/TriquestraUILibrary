9791308abbbf36217392400668ef07e2b103400a
DoTin <doquoctin94@gmail.com>
2023-01-05 14:02:37 +0700

    add progress bar line

---
+++ src/Infinity.Blazor.UIControls/ProgressBars/ProgressBarLine.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Components;
+
+namespace Infinity.Blazor.UIControls.ProgressBars;
+
+partial class ProgressBarLine
+{
+    [Parameter]
+    public string Class { get; set; }
+
+    [Parameter]
+    public double Max { get; set; } = 100;
+
+    [Parameter]
+    public double Value { get; set; }
+
+
+    [Parameter]
+    public ProgressBarOrientation Orientation { get; set; } = ProgressBarOrientation.Horizontal;
+}
